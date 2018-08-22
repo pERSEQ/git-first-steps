96bb34279597db53c18956821c5558b45d4356c8
Sergey Lebedev <perseq603@gmail.com>
2018-08-22 13:23:50 +0800

    Ели как сделал рабочие цифры

--- main.js
+++ main.js
@@ -13,9 +13,6 @@ backspaceButton.onclick = () => {
     }
 };
 
-num1.onclick = () => {
-    enterNumber(1)
-};
 
 //
 // const num2 = document.getElementById("num2");
@@ -24,73 +21,24 @@ num1.onclick = () => {
 //     enterNumber(2)
 // };
 
-const num2 = document.getElementById("num2");
 
-num2.onclick = () => {
-    enterNumber(2);
+enterNumber = (n) => {
+    if (input.value === '0') {
+        input.value = n;
+    } else {
+        input.value = input.value + n;
+    }
+};
 
+const numButtons = document.getElementsByClassName('num-button')
 
-    const num3 = document.getElementById("num3");
-
-    num3.onclick = () => {
-        enterNumber(3);
-
-
-        const num4 = document.getElementById("num4");
-
-        num4.onclick = () => {
-            enterNumber(4);
-
-
-            const num5 = document.getElementById("num5");
-
-            num5.onclick = () => {
-                enterNumber(5);
-
-
-                const num6 = document.getElementById("num6");
-
-                num6.onclick = () => {
-                    enterNumber(6);
-
-
-                    const num7 = document.getElementById("num7");
-
-                    num7.onclick = () => {
-                        enterNumber(7);
-
-
-
-                        const num8 = document.getElementById("num8");
-
-                        num8.onclick = () => {
-                            enterNumber(8);
-
-
-
-                            const num9 = document.getElementById("num9");
-
-                            num9.onclick = () => {
-                                enterNumber(9);
-
-
-
-                                const num0 = document.getElementById("num0");
-
-                                num0.onclick = () => {
-                                    enterNumber(0);
-
-
-
-
-
-    enterNumber = (n) => {
-        if (input.value === '0') {
-            input.value = n;
-        } else {
-            input.value = input.value + n;
-        }
-    };
+for (let i = 0; i < numButtons.length; i++) {
+    const button = numButtons[i];
+    const num = button.textContent;
+    button.onclick = () => {
+        enterNumber(num)
+    }
+}
 
 
 // DOM- DOCUMENT OBJECT MODEL
