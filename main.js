be0d28a08bb079f0a718f7a996eedf862a3948cc
Sergey Lebedev <perseq603@gmail.com>
2018-08-23 13:19:27 +0800

    Ели как сделал рабочие цифры

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log(num1);
 
 const input = document.getElementById("num-input");
 
-const backspaceButton = document.getElementById('backspace')
+const backspaceButton = document.getElementById('backspace');
 backspaceButton.onclick = () => {
     if (input.value.length === 1) {
         input.value = '0';
@@ -30,7 +30,7 @@ enterNumber = (n) => {
     }
 };
 
-const numButtons = document.getElementsByClassName('num-button')
+const numButtons = document.getElementsByClassName('num-button');
 
 for (let i = 0; i < numButtons.length; i++) {
     const button = numButtons[i];
@@ -39,6 +39,33 @@ for (let i = 0; i < numButtons.length; i++) {
         enterNumber(num)
     }
 }
+
+
+const dotButton = document.getElementById('dotButton');
+
+dotButton.onclick = () => {
+const n = input.value.length ;
+for (let i = 0; i < input.value.length; i++){
+    if (input.value[i]==='.'){
+        return;
+    }
+}
+input.value+='.'
+};
+
+let num =null;
+let op =null;
+const plusButton = document.getElementById('plusButton');
+plusButton.onclick = () => {
+    if(num==null){
+        op='+';console.log('+');
+        return
+    }
+
+};
+
+
+
 
 
 // DOM- DOCUMENT OBJECT MODEL
